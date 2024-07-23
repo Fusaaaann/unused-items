01da54fe08e2050c789defa06625d1e1b56c29a1
Fusaaaann <fusen.5065@gmail.com>
2024-07-23 15:22:25 +0800

    fix tag aggregation method
    Signed-off-by: Fusaaaann <fusen.5065@gmail.com>

--- src/components/holding.vue
+++ src/components/holding.vue
@@ -113,7 +113,7 @@ onBeforeMount(()=>{
     tableData.value = res;
     tagsRef.value = [];
     for(let item in tableData.value){
-      tagsRef.value.push(item['tag'])
+      tagsRef.value.concat(item['tag'])
     }
     })
 })
